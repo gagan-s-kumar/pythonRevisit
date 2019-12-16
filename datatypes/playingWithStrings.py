ca214f77822e8c17e5e83eda7b8e23643ca8a9bd
Gagan Shantha Kumar <gaganskumar9@gmail.com>
2019-12-16 18:29:11 -0500

    Playing with Strings

---
+++ datatypes/playingWithStrings.py
@@ -0,0 +1,8 @@
+phrase = "Gagan Kumar"
+print(phrase + " is cool")
+print(phrase.lower())
+print(phrase.upper().isupper())
+print("Length of the phrase is", len(phrase))
+print(phrase[0] + " is at the ", phrase.index(phrase[0]) , " position.")
+print(phrase.replace("Gagan", "Shantha"))
+
