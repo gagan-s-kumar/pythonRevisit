9ac05fd673d9ae20702a610e2b257e43868d2fe2
Gagan Shantha Kumar <gaganskumar9@gmail.com>
2019-12-16 18:40:47 -0500

    Playing with Numbers

---
+++ datatypes/playingWithNumbers.py
@@ -0,0 +1,12 @@
+from math import *
+
+my_num = -5
+print("The number is", my_num)
+print("The String Number is " + str(my_num))
+print("Absolute value is ", abs(my_num))
+print("The square is ", pow(my_num, 2))
+print("The larger number between 4 and 6 is ", max(4, 6))
+print("Rounded up number of 3.142 is ", round(3.142))
+print("Flooring of pie is ", floor(3.142))
+print("Ceil of pie is ", ceil(3.142))
+print("Square root of pie is ", sqrt(3.142))
